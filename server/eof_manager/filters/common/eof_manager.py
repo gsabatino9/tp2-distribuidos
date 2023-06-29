9d2ddc56f96b613a9502688d7ff1e82433ff9ff3
Gonzalo Sabatino <gonzalosabatino@gsabatino.local>
2023-06-29 13:51:07 -0300

    abort en filters

--- server/eof_manager/filters/common/eof_manager.py
+++ server/eof_manager/filters/common/eof_manager.py
@@ -78,7 +78,7 @@ class EOFManager:
         header = decode(body)
         self.state.verify_client(header.id_client)
 
-        if is_eof(header):
+        if is_eof(header) or is_abort(header):
             self.__send_eofs(header, body)
         else:
             self.__recv_ack_trips(header, body)
